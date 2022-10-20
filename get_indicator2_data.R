5c863ce11f5b30268ef011b4e6327112254cca7d
AliciaMstt <ticatla@gmail.com>
2022-10-20 10:21:46 -0500

    finish get_indicator2_data function

--- get_indicator2_data.R
+++ get_indicator2_data.R
@@ -1,8 +1,8 @@
 ###
 ### This R function takes as input the output of the Kobo form "International Genetic Indicator testing" 
-### and reformat its in order to have the data in a table useful for estimating 
+### and reformat its in order to have the data in a dataframe useful for estimating 
 ### Genetic Diversity Indicator 2 (the proportion of populations within species which are maintained)
-###
+### 
 
 ### If you use this script, please cite XXXXXXXX
 
@@ -10,19 +10,21 @@
 ### Libraries
 ###
 
-library(tidyr)
-library(dplyr)
-library(utile.tools)
+get_indicator2_data<-function(file=file){
 
-#file="kobo_output.csv"
+### Arguments:
 
-get_indicator2_data<-function(file=file){}
-
-#### Load data
-
-# Input data should had been exported from Kobotoolbox and saved as .csv
+# file = path to the .csv file with kobo output. This input data should had been exported 
+# from Kobotoolbox and saved as .csv
 # as detailed in the README at: XXX UPDATE URL TO FINAL GITHUB REPO HERE".
+  
+### Needed libraries:  
+  
+#  library(tidyr)
+#  library(dplyr)
+#  library(utile.tools)
 
+### Function  
 # Read data
 kobo_output<-read.csv(file, sep=";", header=TRUE)
 
@@ -38,9 +40,19 @@ indicator2_data<-kobo_output %>%
                  mutate(year_assesment=substr(end,1,4)) %>%
   
                  ## select relevant columns 
-                 # taxon and assesment info
+                 # taxon and assessment info
                  dplyr::select(country_assessment, taxonomic_group, taxon, scientific_authority,
                                genus, taxon, year_assesment, name_assessor, email_assessor,
                  # indicator 2 data               
-                               n_extant_populations, n_extint_populations, other_populations, time_populations,
-                               defined_populations, source_definition_populations)
+                               n_extant_populations, n_extint_populations, other_populations, time_populations) %>%
+                 # change -999 to Na
+                         mutate(n_extint_populations=na_if(n_extint_populations, -999), 
+                                n_extant_populations=na_if(n_extant_populations, -999))
+                # print data
+                print(indicator2_data)
+                
+                # End of function
+                           }
+                
+
+
