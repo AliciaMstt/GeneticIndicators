46c5c434e513cda505550449e7deda323f79d51d
Fumiko Ishihama (for work) <98511549+ishihamaF@users.noreply.github.com>
2023-05-02 16:12:57 +0900

    A script to calculate the fragmentation index from table
    Small corrections for duplicate codes and value of example data to avoid zero NFI

--- calc_FragmentationIndex_table.R
+++ calc_FragmentationIndex_table.R
@@ -1,7 +1,7 @@
 # create example data 
 
 points <- data.frame(rec_name = paste("record", c(1:4), sep=""), 
-			x = c(1, 2, 3, 4), y = c(1, 3, 2, 4), Ne = c(10, 20, 15, 25)) 
+			x = c(1, 2, 3, 4), y = c(1, 3, 2, 4), Ne = c(100, 200, 350, 250)) 
 print(points)
  
 coord_col <- c("x","y")
@@ -11,7 +11,7 @@ coord_col <- c("x","y")
 disp_dist <- 2 
 
 
-# Calculate differences between coordinates
+# Calculate pairwise differences between coordinates
 
 n_points <- dim(points)[1]
 dim_coord <- length(coord_col)
@@ -28,17 +28,10 @@ dists <- matrix(dists, dim(points)[1])
 
 # Calculate neighborhood Ne 
 
-neighbor_Nes <- colSums(points$Ne * (dists < disp_dist)) 
-names(neighbor_Nes) <- points$pop_name
+neighbor_Ne <- colSums(points$Ne * (dists < disp_dist)) 
+names(neighbor_Ne) <- points$rec_name
 
-print(neighbor_Nes)
-
-# Calculate the neighbor Ne 
-
-neighbor_Ne <- colSums(points_df$Ne * dist_weight)
-names(neighbor_Ne) <- points_df$rec_name
-
-print(neighbor_Ne) 
+print(neighbor_Ne)
 
 # Calcurate the Ne fragmentation index
 
