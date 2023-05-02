5b2c4b657fb2a3e013e039aed03e413599173456
Fumiko Ishihama (for work) <98511549+ishihamaF@users.noreply.github.com>
2023-05-02 15:59:54 +0900

    A script to calculate the fragmentation index from GIS data
    A script to calculate the fragmentation index from GIS data

--- calc_FragmentationIndex_poly.R
+++ calc_FragmentationIndex_poly.R
@@ -80,7 +80,10 @@ print(neighbor_Ne)
 
 ##### Calcurate the Ne fragmentation index
 
-sum(neighbor_Ne>500)/length(neighbor_Ne)
+NFI <- sum(neighbor_Ne>500)/length(neighbor_Ne)
 # it should be > 5000 when the population size is Nc
+
+print(NFI)
+
 
 
