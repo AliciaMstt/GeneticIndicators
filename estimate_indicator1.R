077db722164191ef0fdce423f080b80ba1b14e01
AliciaMstt <ticatla@gmail.com>
2023-11-03 07:40:29 +0100

    improve documentation

--- estimate_indicator1.R
+++ estimate_indicator1.R
@@ -1,5 +1,6 @@
 estimate_indicator1<- function(ind1_data){
-### This functions estimates the Ne 500 indicator, ie for each assessment of a taxon it calculates the proportion of populations within it which are above Ne 500
+### This functions estimates the Ne 500 indicator, ie for each assessment of a taxon it calculates the proportion of populations within it which are above Ne 500. Notice it uses the assessment id `X_uuid` (unique record of a taxon), because a single taxon could be assessed by different countries
+##  or more than once with different parameters. The output is a new dataframe with a row per assessment, metadata, new columns used to estimate the indicator (number of pops) and the indicator value.
   
 ## Arguments
 ## ind1_data: population size data as produced by get_indicator1_data() and after running transform_to_Ne()
